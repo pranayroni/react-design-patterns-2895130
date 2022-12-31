30c1e4a851608fef24a1f80cd470acfa079676ea
Pranay Roni <pranay.roni@icloud.com>
2022-12-31 17:56:03 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import {CurrentUserLoader} from './CurrentUserLoader';
+import {UserInfo} from './UserInfo';
+import {UserLoader} from './UserLoader';
+import {ResourceLoader} from './ResourceLoader'
+import {ProductInfo} from './ProductInfo';
+import {DataSource} from './DataSource';
+import axios from "axios";
+const getServerData = url => async () => {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+const getLocalStorageData = key => () => {
+  return localStorage.getItem(key);
+}
+
+const Text = ({message}) => <h1>{message}</h1>
+
 function App() {
 	return (
-		<h1>Your code goes here</h1>
+		<>
+    <DataSource getDataFunction={getServerData('/users/123')} resourceName="user" >
+      <UserInfo />
+    </DataSource>
+    <DataSource getDataFunction={getServerData('/products/1234')} resourceName="product" >
+      <ProductInfo />
+    </DataSource>
+    <DataSource getDataFunction={getLocalStorageData('message')} resourceName="message">
+      <Text />
+    </DataSource>
+    </>
 	);
 }
 
